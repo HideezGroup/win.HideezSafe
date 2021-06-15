827a67ebe3b374da53f253d560e84f4c3b1718c0
Vlad Panasiuk <pv.hideez@gmail.com>
2021-06-15 10:38:11 +0300

    Added users dropdown in proximity settings when there are more than one

---
+++ src/HideezClient/HideezClient/Converters/FullUserNameToOnlyNameConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace HideezClient.Converters
+{
+    class FullUserNameToOnlyNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string str = (string)value;
+            return str?.Split('\\')[1];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+    }
+}
