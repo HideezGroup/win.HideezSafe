1e61c993b4ae0c7f2072b6e0df1b87add05189d2
Vlad Panasiuk <pv.hideez@gmail.com>
2021-07-08 18:26:22 +0300

    Simplified the logic of sample

--- src/Samples/ButtonPressTrackingSample/ButtonPressTrackingSample/Program.cs
+++ src/Samples/ButtonPressTrackingSample/ButtonPressTrackingSample/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Meta.Lib.Modules.PubSub;
 using Meta.Lib.Modules.PubSub.Messages;
-using Meta.Lib.Utils;
 using Nito.AsyncEx;
 using System;
 using System.Threading.Tasks;
@@ -13,9 +12,6 @@ namespace ButtonPressTrackingSample
     class Program
     {
         static IMetaPubSub _metaMessenger = new MetaPubSub();
-        static TaskCompletionSource<DeviceDTO> _connectionTcs = new TaskCompletionSource<DeviceDTO>();
-        static TaskCompletionSource<bool> _buttonPressTcs = new TaskCompletionSource<bool>();
-
         static void Main(string[] args)
         {
             AsyncContext.Run(() => MainAsync(args));
@@ -30,63 +26,21 @@ namespace ButtonPressTrackingSample
                 await _metaMessenger.TrySubscribeOnServer<DeviceConnectionStateChangedMessage>(OnDeviceConnectionStateChanged);
                 await _metaMessenger.TrySubscribeOnServer<DeviceStateChangedMessage>(OnDeviceStateChanged);
                 await _metaMessenger.TryConnectToServer("HideezServicePipe");
-
-                var connectedDevice = await WaitConnection();
-                if (connectedDevice != null)
-                {
-                    Console.WriteLine($"Connected device is {connectedDevice.Name}");
-
-                    var pressedBtn = await WaitButtonPress();
-                    if (!pressedBtn)
-                        Console.WriteLine("No button was detected");
-                }
-                else
-                    Console.WriteLine("No connected devices");
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
 
-            Console.WriteLine("Press any key...");
+            Console.WriteLine("Press 'Enter' to exit");
             Console.ReadLine();
         }
 
         private static Task OnDeviceStateChanged(DeviceStateChangedMessage arg)
         {
             if (arg.Button != ButtonPressCode.None)
-            {
                 Console.WriteLine($"Vault {arg.DeviceId} button pressed, code: {arg.Button}");
-                _buttonPressTcs.TrySetResult(true);
-            }
             return Task.CompletedTask;
-        }
-
-        static async Task<DeviceDTO> WaitConnection()
-        {
-            Console.WriteLine("Waiting connection...");
-            try
-            {
-                return await _connectionTcs.Task.TimeoutAfter(30000);
-            }
-            catch
-            {
-                return null;
-            }
-
-        }
-
-        static async Task<bool> WaitButtonPress()
-        {
-            Console.WriteLine("Waiting for button press..");
-            try
-            {
-                return await _buttonPressTcs.Task.TimeoutAfter(60000);
-            }
-            catch
-            {
-                return false;
-            }
         }
 
         static Task OnDevicesCollectionChanged(DevicesCollectionChangedMessage message)
@@ -94,7 +48,10 @@ namespace ButtonPressTrackingSample
             foreach (var deviceDto in message.Devices)
             {
                 if (deviceDto.IsConnected)
-                    _connectionTcs.TrySetResult(deviceDto);
+                {
+                    Console.WriteLine($"Connected device is {deviceDto.Name}");
+                    return Task.CompletedTask;
+                }
             }
 
             return Task.CompletedTask;
@@ -103,8 +60,7 @@ namespace ButtonPressTrackingSample
         static Task OnDeviceConnectionStateChanged(DeviceConnectionStateChangedMessage message)
         {
             Console.WriteLine($"Device {message.Device.Mac} connection state changed: IsConnected - {message.Device.IsConnected}");
-            if (message.Device.IsConnected)
-                _connectionTcs.TrySetResult(message.Device);
+            
             return Task.CompletedTask;
         }
 
